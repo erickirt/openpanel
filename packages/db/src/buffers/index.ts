117ba4b52f6715b0d9b3495abd0cfa86a7ff26e5
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2025-02-11 21:38:26 +0100

    fix(buffer): live counter

--- packages/db/src/buffers/index.ts
+++ packages/db/src/buffers/index.ts
@@ -1,16 +1,7 @@
-import { BotBuffer as BotBufferPsql } from './bot-buffer-psql';
 import { BotBuffer as BotBufferRedis } from './bot-buffer-redis';
-import { EventBuffer as EventBufferPsql } from './event-buffer-psql';
 import { EventBuffer as EventBufferRedis } from './event-buffer-redis';
-import { ProfileBuffer as ProfileBufferPsql } from './profile-buffer-psql';
 import { ProfileBuffer as ProfileBufferRedis } from './profile-buffer-redis';
 
-export const eventBuffer = process.env.USE_NEW_BUFFER
-  ? new EventBufferRedis()
-  : new EventBufferPsql();
-export const profileBuffer = process.env.USE_NEW_BUFFER
-  ? new ProfileBufferRedis()
-  : new ProfileBufferPsql();
-export const botBuffer = process.env.USE_NEW_BUFFER
-  ? new BotBufferRedis()
-  : new BotBufferPsql();
+export const eventBuffer = new EventBufferRedis();
+export const profileBuffer = new ProfileBufferRedis();
+export const botBuffer = new BotBufferRedis();
